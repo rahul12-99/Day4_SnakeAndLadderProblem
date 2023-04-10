f98f8e5c65af4fcaa6446b0c769694a4a42d17ff
rahul12-99/rahul12-99 <rahulkrrao162@gmail.com>
2023-04-10 09:40:02 +0530

    Added : Code for printing the player position at 0

---
+++ src/com/snakeandladder/SnakeAndLadderSimulator.java
@@ -0,0 +1,17 @@
+package com.snakeandladder;
+
+public class SnakeAndLadderSimulator {
+    /**
+     * Created method for printing the player position at 0;
+     * @param args
+     */
+
+    public static void main(String[] args) {
+        /*
+         * 1) initialization the player position and printing;
+         */
+
+        int playerPosition = 0;
+        System.out.println("Player position is : " + playerPosition);
+    }
+}
