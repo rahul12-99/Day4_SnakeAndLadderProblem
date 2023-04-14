49566bd706a96c63754b99d3fc0832665485bd4c
rahul12-99/rahul12-99 <rahulkrrao162@gmail.com>
2023-04-14 09:10:05 +0530

    Added : Code for getting the number between 1 to 6

--- src/com/snakeandladder/SnakeAndLadderSimulator.java
+++ src/com/snakeandladder/SnakeAndLadderSimulator.java
@@ -1,17 +1,19 @@
 package com.snakeandladder;
 
 public class SnakeAndLadderSimulator {
-    /**
-     * Created method for printing the player position at 0;
+    /*
+     * Created method for player role the dice and get number between 1 and 6 ;
      * @param args
      */
 
     public static void main(String[] args) {
         /*
-         * 1) initialization the player position and printing;
+         * 1) initialization the player position and a random variable to get number 1 to 6;
          */
 
         int playerPosition = 0;
-        System.out.println("Player position is : " + playerPosition);
+        int playerGetNumber = (int) Math.floor(Math.random()*(6 -1 +1) + 1);
+        System.out.println("Player position is : " + playerPosition );
+        System.out.println("Player rolls the die and get " + playerGetNumber);
     }
 }
