4fc758f31ee1aa6970a7d6322aae671b4e4c1bd8
rahul12-99/rahul12-99 <rahulkrrao162@gmail.com>
2023-05-08 18:35:08 +0530

    Added : Code for player position repeat till reach 100

--- src/com/snakeandladder/SnakeAndLadderSimulator.java
+++ src/com/snakeandladder/SnakeAndLadderSimulator.java
@@ -2,7 +2,7 @@ package com.snakeandladder;
 
 public class SnakeAndLadderSimulator {
     /*
-     * The main method is for Player then checks for an Option. They are No Play, Ladder or Snake.
+     * The main method is for Player repeat till player position reach the 100
      *  - Use ((RANDOM)) to check for Options
      * @param args
      */
@@ -10,27 +10,40 @@ public class SnakeAndLadderSimulator {
     public static void main(String[] args) {
         /*
          * 1) Initialized the playerPosition and random variable for getNumber and get options for play.
+         * 1.1) Taking a condition in while loop until 100
          * 2) Taking condition for no play, and ladder or snake
+         * 3) Taking condition if player position less than 0
          */
         /*
          * 1) Initialized the playerPosition and random variable for getNumber and get options for play.
          */
         int playerPosition = 0;
-        int getNumber = (int) Math.floor(Math.random() * (6 - 1 + 1) + 1);
-        int getOption = (int) Math.floor(Math.random() * (3 - 1 + 1) + 1);
+        /*
+         1.1) Taking a condition in while loop until 100
+         */
+        while (playerPosition < 100) {
+            int getNumber = (int) Math.floor(Math.random() * (6 - 1 + 1) + 1);
+            int getOption = (int) Math.floor(Math.random() * (3 - 1 + 1) + 1);
         /*
          2) Taking condition for no play, and ladder or snake
          */
-        switch (getOption) {
-            case 0:
-                playerPosition = playerPosition;
-                break;
-            case 1:
-                playerPosition += getNumber;
-                break;
-            case 2:
-                playerPosition -= getNumber;
-                break;
+            switch (getOption) {
+                case 0:
+                    playerPosition = playerPosition;
+                    break;
+                case 1:
+                    playerPosition += getNumber;
+                    break;
+                case 2:
+                    playerPosition -= getNumber;
+                    break;
+            }
+            /*
+             3) Taking condition if player position less than 0
+             */
+            if(playerPosition < 0){
+                playerPosition = 0;
+            }
         }
         System.out.println(playerPosition);
     }
